c52fc995bb76159de2e3a9593dc5b821106790bf
Gaurav Agarwal <algogrit@gmail.com>
2020-03-31 15:02:17 +0530

    Reseting scripts.

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -5,6 +5,9 @@ set -e
 docker ps -aq | xargs -n 1 docker stop # Stop all containers
 docker ps -aq | xargs -n 1 docker rm # Remove all containers
 
-./scripts/docker-clean-up.sh
+# Remove all images
+# docker images -q | xargs -n 1 docker rmi
+
+./scripts/clean-up.sh
 
 docker network prune -f
