d49859ac66f9f4f76ce6bef4b78d9e448e679175
Gaurav Agarwal <algogrit@gmail.com>
2020-03-31 12:11:17 +0530

    Adding a reset script.

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
 docker stop $(docker ps -aq) # Stop all containers
 docker rm $(docker ps -aq) # Remove all containers
+
+./scripts/docker-clean-up.sh
+
+docker network prune -f
