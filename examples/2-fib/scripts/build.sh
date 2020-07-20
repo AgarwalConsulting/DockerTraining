cbc91b9664882f112b2168fe1577265dab03eea8
Gaurav Agarwal <algogrit@gmail.com>
2020-07-20 19:53:11 +0530

    Adding scripts to build & run.

---
+++ examples/2-fib/scripts/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker build -t agarwalconsulting/fib-gen .
+docker push agarwalconsulting/fib-gen
---
+++ examples/2-fib/scripts/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run -it --rm agarwalconsulting/fib-gen
