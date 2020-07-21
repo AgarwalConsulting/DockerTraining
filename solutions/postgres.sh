fca3fe62ebb3f60e62c3b8b584d4034c850900a7
Gaurav Agarwal <algogrit@gmail.com>
2020-07-21 10:32:35 +0530

    Using docker managed volume.

--- solutions/postgres.sh
+++ solutions/postgres.sh
@@ -3,6 +3,6 @@
 docker run --rm -it \
   -e POSTGRES_PASSWORD=password \
   -e PGDATA=/var/lib/postgresql/data/pgdata \
-  -v `pwd`/experiment/postgres:/var/lib/postgresql/data \
+  -v postgres:/var/lib/postgresql/data \
   -p 5433:5432 \
   postgres
