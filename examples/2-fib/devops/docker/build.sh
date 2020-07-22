49860931aaafa3f25631e6523dd3d0f6083a3d80
Gaurav Agarwal <algogrit@gmail.com>
2020-07-22 11:53:35 +0530

    Creating a alpine flavor.

--- examples/2-fib/devops/docker/build.sh
+++ examples/2-fib/devops/docker/build.sh
@@ -2,3 +2,6 @@
 
 docker build -t agarwalconsulting/fib-gen -f Dockerfile-Optimized .
 docker push agarwalconsulting/fib-gen
+
+docker build -t agarwalconsulting/fib-gen:alpine --build-args FLAVOR=alpine .
+docker push agarwalconsulting/fib-gen:alpine
