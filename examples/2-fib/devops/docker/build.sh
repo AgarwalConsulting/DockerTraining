9098709cf3afc9f2bc245c0824391e2e2943e891
Gaurav Agarwal <algogrit@gmail.com>
2020-07-22 11:54:44 +0530

    Fixing the command.

--- examples/2-fib/devops/docker/build.sh
+++ examples/2-fib/devops/docker/build.sh
@@ -3,5 +3,5 @@
 docker build -t agarwalconsulting/fib-gen -f Dockerfile-Optimized .
 docker push agarwalconsulting/fib-gen
 
-docker build -t agarwalconsulting/fib-gen:alpine --build-args FLAVOR=alpine .
+docker build -t agarwalconsulting/fib-gen:alpine --build-arg FLAVOR=alpine .
 docker push agarwalconsulting/fib-gen:alpine
